3f012fb2f8c84ac03a54878b70c46ff6b6e7177f
wzq <wzqac2017@163.com>
2020-08-21 15:56:31 +0800

    fix uploadUrl

--- server/routers/admin/index.js
+++ server/routers/admin/index.js
@@ -51,7 +51,7 @@ module.exports = app => {
   const upload = multer({dest: __dirname + '/../../uploads'})
   app.post('/admin/api/upload', authMiddleware(),upload.single('file'),async (req,res)=>{
     const file = req.file
-    file.url = `http://localhost:3000/uploads/${file.filename}`
+    file.url = `http://test.jccwine.top/uploads/${file.filename}`
     res.send(file)
   })
 
